07eab82f8094f7305965d0999eda060aa06a59eb
Dumitru Pascu <dumitru.pascu@swisscom.com>
2020-08-20 08:56:06 +0300

    Adding Oracle DB support, WIP

--- src/main/java/io/mindit/dockerdemo/User.java
+++ src/main/java/io/mindit/dockerdemo/User.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity // This tells Hibernate to make a table out of this class
+@Table(name = "DEMO_USER")
 public class User {
 
     @Id
