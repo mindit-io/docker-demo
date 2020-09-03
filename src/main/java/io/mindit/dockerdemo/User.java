77eb00d4dba1b100f6d08121d50cf73c374f7d19
Dumitru Pascu <dumitru.pascu@swisscom.com>
2020-09-03 15:27:01 +0300

    Cleaned up unnecessary comments

--- src/main/java/io/mindit/dockerdemo/User.java
+++ src/main/java/io/mindit/dockerdemo/User.java
@@ -5,9 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import lombok.Data;
 
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "DEMO_USER")
+@Data
 public class User {
 
     @Id
@@ -17,29 +19,5 @@ public class User {
     private String name;
 
     private String email;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 
 }
